049a015f2c41099be12381b3fd4377ad40e3aa35
dalcubierre <diegoalcubierre@hotmail.com>
2022-01-08 21:27:37 -0500

    Initial Commit

---
+++ Quotes Test/Quotes_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quotes_TestApp.swift
+//  Quotes Test
+//
+//  Created by Alcubierre Holdings Inc. on 2022-01-08.
+//
+
+import SwiftUI
+
+@main
+struct Quotes_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
